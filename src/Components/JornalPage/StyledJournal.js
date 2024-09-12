d04e0a4179bf98211c2be205c25113df04c3cab1
Jake7463 <jacobhick@gmail.com>
2024-09-12 16:46:04 +0300

    stylized base journal page for mobile view

--- src/Components/JornalPage/StyledJournal.js
+++ src/Components/JornalPage/StyledJournal.js
@@ -1,49 +1,64 @@
 import styled from "styled-components";
 
 const StyledJSpan = styled.span`
-    @media (min-width: 768.1px){
+    @media (min-width: 481.1px){
         width: 75vw;
         border-radius: 13px;
         display: grid;
         grid-template-columns: 0.03fr 0.15fr 0.53fr 0.02fr 0.02fr 0.1fr 0.1fr 0.1fr;
         grid-template-areas: "chrono date name lucid nightmare positivity pride personal-score";
-        grid-gap: 5px;
+        grid-gap: 15px;
+    }
+    @media (max-width: 480px){
+        width: 88vw;
+        border-radius: 9px;
+        display: grid;
+        grid-template-columns: 0.8fr 1.5fr;
+        grid-template-areas: "date name";
     }
 `;
 
-const StyledJChrono = styled.p`
-    grid-area: chrono;
+const StyledJDate =styled.p`
+    grid-area: date;
     background-color: #265cae;
     text-align: center;
-    padding: 2px 17px;
+    padding: 2px 7px;
     border-radius: 8px;
     margin: 5px 10px;
     align-self: center;
 `;
-const StyledJDate =styled(StyledJChrono)`
-    grid-area: date;
+
+const StyledJChrono = styled(StyledJDate)`
+    grid-area: chrono;
+    @media (max-width: 480px){
+        display: none;
+    }
 `;
-const StyledJName =styled(StyledJChrono)`
+
+const StyledJName =styled(StyledJDate)`
     grid-area: name;
+    text-overflow: ellipsis; 
+    overflow: hidden; 
+    white-space: nowrap;
 `;
 
 const StyledJL =styled(StyledJChrono)`
     grid-area: lucid;
 `;
 
-const StyledJN =styled(StyledJChrono)`
+const StyledJN =styled(StyledJL)`
     grid-area: nightmare;
 `;
 
-const StyledJPos =styled(StyledJChrono)`
+const StyledJPos =styled(StyledJL)`
     grid-area: positivity;
 `;
 
-const StyledJPride =styled(StyledJChrono)`
+const StyledJPride =styled(StyledJL)`
     grid-area: pride;
 `;
 
-const StyledJPersonal =styled(StyledJChrono)`
+const StyledJPersonal =styled(StyledJL)`
     grid-area: personal-score;
 `;
 
